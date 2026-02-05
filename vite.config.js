714ccd224db5cafc1970704796388bc534b81e83
avaong <avawang821@gmail.com>
2026-02-05 23:29:54 +0800

    Update base path for root deployment

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // 注意：如果您部署到 https://<USERNAME>.github.io/ava-portfolio/
-  // 请保持 base 为 '/ava-portfolio/'。
-  // 如果是自定义域名根目录，请改为 '/'
-  base: '/ava-portfolio/',
+  // 注意：如果您部署到 https://<USERNAME>.github.io/
+  // 请保持 base 为 '/'。
+  // 如果是子目录部署，请改为 '/subdirectory/'
+  base: '/',
 })
